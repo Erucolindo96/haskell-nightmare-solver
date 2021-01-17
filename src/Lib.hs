3279a74d78ff61a5515b2ae3601f935b16cf8fac
Krzysztof Dziurlikowski <krzysztof.dziurlikowski@atendesoftware.pl>
2021-01-17 18:29:51 +0100

    Add basic solver with bugs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,9 @@
 module Lib
-    ( someFunc, getSquare
+    ( someFunc, getSquare, fill, returnSquareIndexes
     ) where
+
+import Data.Char (digitToInt)
+
 
 s :: Int
 s = 5
@@ -15,11 +18,11 @@ rBottom = s^2 - 1
 
   
 exampleStr :: [Char]
-exampleStr =  "0.44."++
-              ".4.6."++
-              "3.76."++
-              ".6.65"++
-              "....3"
+exampleStr =  "...1"++
+              ".9..."++
+              ".88.."++
+              "....4"++
+              "4.5.2"
 resultStr :: [Char]
 resultStr  = "nnnnn"++
              "nnnnn"++
@@ -28,8 +31,10 @@ resultStr  = "nnnnn"++
              "nnnnn"
 
 someFunc :: IO ()
---someFunc = print (solve exampleStr 0 25 resultStr)
-someFunc = print (getSquare rBottom s resultStr 'b')
+                    
+--someFunc = print (getSquare rBottom s resultStr 'b')
+someFunc = print (solve exampleStr 0 25 resultStr)
+
 example :: [[Char]]
 example = 
   [
@@ -55,8 +60,62 @@ square_size = 5
 count :: Eq a => a -> [a] -> Int
 count x = length . filter (x==) 
  
+
+returnSquareIndexes :: Int -> Int -> [Int]
+returnSquareIndexes i s_size
+  | i == leftTop = --jestes w lewym górnym rogu
+    [i, i+1, i+s_size, i+(s_size+1)]
+  | i == rightTop = --jestes w prawym gornym rogu
+    [i-1, i, i+(s_size-1),i+s_size]
+  | i == leftBottom = -- jestes w lewym dolnym  
+    [i-s_size, i-s_size + 1, i, i+1]
+  | i == rightBottom = --jestes w prawym dolnym rogu
+    [ i-s_size-1, i-s_size ,i-1, i]
+  | i > leftTop && i < rightTop = --jestes w gornej czesci
+    [i-1, i, i+1,i+(s_size-1), i+s_size, i+(s_size+1)]
+  | i > leftBottom && i < rightBottom = --jestes w dolnej czesci
+    [ i-s_size-1, i-s_size, i-s_size+1, i-1, i, i+1]
+  | i `mod` s_size == 0 = --jestes w lewej czesci
+    [i-s_size, i-s_size+1, i, i+1, i+s_size, i +(s_size+1)]
+  | i `mod` s_size == s_size - 1 =  -- jestes w prawej czesci
+    [i-s_size-1, i-s_size, i-1, i, i+(s_size-1), i+s_size]
+  | otherwise =  --jestes w srodku macierzy
+    [i-s_size-1, i-s_size, i-s_size+1, i-1, i, i+1, i+(s_size-1), i+s_size, i+(s_size+1)]
+  where
+    leftTop = 0 
+    leftBottom = s_size * (s_size - 1)
+    rightTop = s_size - 1
+    rightBottom = s_size^2 - 1
+
+fill :: Char -> Int -> Int -> [Int] -> [Char] -> [Char]    
+fill _ _ _ _ [] = []    
+fill c i size indexes (r:restResults)
+ | i `elem` indexes && r == 'n' = c:fill c (i+1) size indexes restResults
+ | otherwise = r:fill c (i+1) size indexes restResults
+
+
+     
 -- dostajesz kwadrat 3x3 i result - sprobuj go pokolorować  
-processOneSquare square square_result result = result
+processOneSquare :: Int -> Int -> [Char] -> [Char] -> [Char] -> [Char]
+processOneSquare i s_size square square_result result 
+ | field_num == 0 = --zamaluj wszystko na bialo
+  fill 'w' i s_size (returnSquareIndexes i s_size) result
+ | field_num == 9 = --zamaluj wszystko na czarno
+  fill 'b' i s_size (returnSquareIndexes i s_size) result
+ | field_num == b_cnt = -- zamaluj wszystkie ny na biało
+  fill 'w' i s_size (returnSquareIndexes i s_size) result
+ | b_n_sum == field_num = --jest tyle pól niepokolorowanych co liczba w srodku kwadratu
+  fill 'b' i s_size (returnSquareIndexes i s_size) result
+ | otherwise = result
+ where 
+  field_num = digitToInt (square!!4) 
+  n_cnt = count 'n' square_result
+  b_cnt = count 'b' square_result
+--  w_cnt = count 'w' square_result
+  b_n_sum = n_cnt + b_cnt
+  
+ 
+ 
 -- zwraca kwadrat 3x3 z plamnszy pix którego środek znajduje się w i. Uzupełnia 
 -- i - srodek kwadratu
 -- s_size - długośc boku całej planszy
@@ -123,20 +182,20 @@ getSquare i s_size pix c
     rightTop = s_size - 1
     rightBottom = s_size^2 - 1
 
-
+--processOneSquare i s_size square square_result result 
 
 --proba rozwiazania zaczynając od elementu x
 solver :: Int -> Int -> [Char] -> [Char] -> [Char]
 solver i n pix result
   | i >= n = result
   | pix!!i == '.' = solver (i+1) n pix result
-  | otherwise = solver (i+1) n pix (processOneSquare (getSquare i square_size pix '.') (getSquare i square_size result 'b') result)
+  | otherwise = solver (i+1) n pix (processOneSquare i n (getSquare i square_size pix '.') (getSquare i square_size result 'b') result)
 
 --glowna fcja programu - bierze "example", wypluwa "result"
 solve :: [Char] -> Int -> Int -> [Char] -> [Char]
 solve pix i n result = if i >= n 
                           then result 
-                            else solve pix (i+1) n (solver i n pix result)
+                            else solve pix (i+1) n (solver 0 n pix result)
 --solve _  result = result
 
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,6 +8,11 @@ test_pix =    "0.44."++
               "3.76."++
               ".6.65"++
               "....3"
+test_results = "nwnnn"++
+               "nnwnn"++
+               "nwbnn"++
+               "nnnnn"++
+               "nnnnn"
 s_size = 5
   
 lTop = 0 
@@ -31,4 +36,30 @@ main = do
   --srodek
   print(Lib.getSquare 16 s_size test_pix '.' == "3.7.6....")  -- 4 wiersz, 2 element
     
-      
+    
+  --metoda fill - zamaluj lewy górny 2x2
+  print(Lib.fill 'b' 0 s_size [0, 1, s_size, s_size+1] test_results  ==  "bwnnn"++
+                                                                         "bbwnn"++
+                                                                         "nwbnn"++
+                                                                         "nnnnn"++
+                                                                         "nnnnn")    
+  
+  --metoda fill - zamaluj srodek 3x3
+  print(Lib.fill 'w' 0 s_size [s_size + 1, s_size + 2, s_size + 3, 
+                               s_size*2 + 1, s_size*2 + 2, s_size*2 + 3,
+                               s_size*3 + 1, s_size*3 + 2, s_size*3 + 3] 
+                               test_results  ==  "nwnnn"++
+                                                 "nwwwn"++
+                                                 "nwbwn"++
+                                                 "nwwwn"++
+                                                 "nnnnn")    
+                                                                
+  -- metoda getSquareIndexes - zwroc prawy górny 2x2
+  print(Lib.returnSquareIndexes 4 s_size == [3,4, s_size+3, s_size+4])
+ 
+  -- metoda getSquareIndexes - zwroc dolny  2x3
+  print(Lib.returnSquareIndexes (s_size*4 + 2) s_size == [s_size*3 + 1,s_size*3 + 2, s_size*3 + 3,
+                                                          s_size*4 + 1,s_size*4 + 2, s_size*4 + 3  
+                                                         ])
+                                                                               
+                                                                         
